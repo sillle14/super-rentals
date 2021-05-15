065ee9552204612881153b2463c255e1a7c078ec
Lewis Silletto <lsilletto@caredash.com>
2021-05-15 12:10:49 -0400

    Add individual rental pages

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -33,6 +33,24 @@ module('Acceptance | super rentals', function (hooks) {
     assert.equal(currentURL(), '/getting-in-touch');
   });
 
+  test('viewing the details of a rental property', async function (assert) {
+    await visit('/');
+    assert.dom('.rental').exists({ count: 3 });
+
+    await click('.rental:first-of-type a');
+    assert.equal(currentURL(), '/rentals/grand-old-mansion');
+  });
+
+  test('visiting /rentals/grand-old-mansion', async function (assert) {
+    await visit('/rentals/grand-old-mansion');
+
+    assert.equal(currentURL(), '/rentals/grand-old-mansion');
+    assert.dom('nav').exists();
+    assert.dom('h1').containsText('SuperRentals');
+    assert.dom('h2').containsText('Grand Old Mansion');
+    assert.dom('.rental.detailed').exists();
+  });
+
   test('visiting /getting-in-touch', async function (assert) {
     await visit('/getting-in-touch');
 

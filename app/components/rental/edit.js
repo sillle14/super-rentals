41a78f5dc86c3906f1b0fc0665fd4ffdaed4797e
Lewis Silletto <lsilletto@caredash.com>
2021-05-16 21:17:02 -0400

    Finalize edit page

--- app/components/rental/edit.js
+++ app/components/rental/edit.js
@@ -1,7 +1,11 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class RentalEditComponent extends Component {
+  @tracked staticTitle;
+  @tracked loading = false;
+
   constructor(...args) {
     super(...args);
     let { rental } = this.args;
@@ -10,12 +14,14 @@ export default class RentalEditComponent extends Component {
   }
 
   @action save() {
+    // TODO: Enable editing the slug.
     let { rental } = this.args;
+    this.loading = true;
     rental
       .save()
       .then(() => {
-        // Reset the title on success.
         this.staticTitle = rental.title;
+        this.loading = false;
       })
       .catch((e) => {
         console.log(e);
